8070aab5970326c0104a6d966469cba57971b389
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-06 00:28:32 +0200

    addCartSlice

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import productDetailsReducer from "./slices/productDetailsSlice";
 import wishlistReducer from "./slices/wishlistSlice";
 import categoryReducer from "./slices/categorySlice";
 
+import cartReducer from "./slices/cartSlice";
 
 export default configureStore({
   reducer: {
     product: productReducer,
     productDetails: productDetailsReducer,
     wishlist: wishlistReducer,
+    cart: cartReducer,
     category: categoryReducer
   },
 });
