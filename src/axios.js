67f22bc2079f706676ca0ead141dc4249cf57d52
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-09 01:47:16 +0200

    add cart slice

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-
+const token = localStorage.getItem('token');
 export const axiosInstance =
     axios.create({
         baseURL: import.meta.env.VITE_API_URL,
+        headers: {
+            Authorization: token ? `Token ${token}` : '', // Include token in the request headers
+            'Content-Type': 'application/json', // Set content type to JSON
+        },
     });
