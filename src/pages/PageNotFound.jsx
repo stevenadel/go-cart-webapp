a0b1de6643962b90d9383de57610271c18ecc22b
Steven Adel <stevenadel.dev@gmail.com>
2024-04-08 11:11:54 +0200

    Add not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,14 @@
 function PageNotFound() {
-  return <div>Page Not Found 🥺🥺</div>;
-}
+  return (
+    <div className="container mx-auto flex items-center justify-center">
+      <div className="row">
+        <h1 className="text-6xl font-bold text-brandYellow">404</h1>
+        <div className="bg-cover h-96" style={{ backgroundImage: "url('public/not-found.gif')" }}></div>
+        <h2 className="text-4xl font-bold text-secondary">Looks like you're lost!</h2>
+        <p className="text-xl text-gray-600 mt-4">The page you're looking for isn't available.</p>
+      </div>
+    </div>
+  );
+};
 
 export default PageNotFound;
