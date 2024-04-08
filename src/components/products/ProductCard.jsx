a9960e527cb62ed82420a2a6705a05c3a5eb4d60
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-09 01:50:17 +0200

    handle add to cart button and display cart

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,51 +1,47 @@
-import React from "react";
-import Button from "../reusables/Button";
 import { useDispatch } from "react-redux";
 import { addToCartThunk } from "../../store/slices/cartSlice";
+import Button from "../reusables/Button";
 
 const ProductCard = ({ data }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const handleAddToCart = (productId) => {
-    dispatch(addToCartThunk(productId)); // Send only productId
-  };
+    const handleAddToCart = (productId) => {
+        dispatch(addToCartThunk({ productId: productId, quantity: 1 }))
+            .then((response) => {
+                console.log("Add to cart successful:", response);
+                // Handle success, if needed
+            })
+            .catch((error) => {
+                console.error("Add to cart failed:", error);
+                // Handle error, if needed
+            });
+    };
 
-  return (
-    <div className="mb-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 place-items-center mt-10 px-8 ">
-        {/* card section */}
-        {data.map((product) => (
-          <div
-            data-aos="fade-up"
-            data-aos-delay={product.aosDelay}
-            className="group"
-            key={product.id}
-          >
-            <div className="relative">
-              <img
-                src={product.image}
-                alt=""
-                className="h-[180px] w-[260px] object-cover rounded-md"
-              />
-              {/* hover button */}
-              <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
-                <Button
-                  text={"Add to cart"}
-                  bgColor={"bg-primary"}
-                  textColor={"text-white"}
-                  handler={() => handleAddToCart(product.id)} // Pass only product id
-                />
-              </div>
+    return (
+        <div className="mb-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 place-items-center mt-10 px-8 ">
+                {data.map((product) => (
+                    <div data-aos="fade-up" data-aos-delay={product.aosDelay} className="group" key={product.id}>
+                        <div className="relative">
+                            <img src={product.image} alt="" className="h-[180px] w-[260px] object-cover rounded-md" />
+                            <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
+                                <Button
+                                    text={"Add to cart"}
+                                    bgColor={"bg-primary"}
+                                    textColor={"text-white"}
+                                    handler={() => handleAddToCart(product.id)}
+                                />
+                            </div>
+                        </div>
+                        <div className="leading-7">
+                            <h2 className="font-semibold">{product.name}</h2>
+                            <h2 className="font-bold">${product.price}</h2>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className="leading-7">
-              <h2 className="font-semibold">{product.name}</h2>
-              <h2 className="font-bold">${product.price}</h2>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default ProductCard;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,14 +4,14 @@ import { getCartThunk } from "../store/slices/cartSlice";
 
 function Cart() {
   const dispatch = useDispatch();
-  const { cartList, isLoading, error, sessionId } = useSelector(
+  const { cartList, isLoading, error } = useSelector(
     (state) => state.cart
   );
 
   // Dispatch getCartThunk action when the component mounts
   useEffect(() => {
-    dispatch(getCartThunk(sessionId)); // Pass session ID to getCartThunk
-  }, [dispatch, sessionId]);
+    dispatch(getCartThunk()); // Pass session ID to getCartThunk
+  }, [dispatch]);
 
   // Render loading state if isLoading is true
   if (isLoading) {
@@ -23,13 +23,36 @@ function Cart() {
     return <div>Error: {error}</div>;
   }
 
-  // Render cart items if cartList is not empty
-  if (cartList) {
-    console.log(cartList);
-    // Render your cart items here
-  }
-
-  return <div>No items in the cart</div>; // Render this if cartList is empty
+  return (
+    <div className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
+      <div className="flex justify-start item-start space-y-2 flex-col">
+        <h1 className="text-3xl dark:text-white lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">Order #13432</h1>
+        <p className="text-base dark:text-gray-300 font-medium leading-6 text-gray-600">21st Mart 2021 at 10:34 PM</p>
+      </div>
+      <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
+        {cartList.map((item) => (
+          <div key={item.id} className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
+            <div className="flex flex-col justify-start items-start dark:bg-gray-800 bg-gray-50 px-4 py-4 md:p-6 md:w-full xl:p-8 w-full">
+              <p className="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">{item.product.name}</p>
+              <div className="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
+                <div className="pb-4 md:pb-8 w-full md:w-40">
+                  <img className="w-full hidden md:block" src={item.product.image} alt={item.product.name} />
+                  <img className="w-full md:hidden" src={item.product.image} alt={item.product.name} />
+                </div>
+                <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
+                  <div className="flex justify-between space-x-8 items-start w-full">
+                    <p className="text-base dark:text-white xl:text-lg leading-6">{`$${item.product.price}`} </p>
+                    <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">{item.quantity}</p>
+                    <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">{`$${item.total}`}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
