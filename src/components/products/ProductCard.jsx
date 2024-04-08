75cd3f70ea31b3941a00c28b2fd9a6053e0cef22
Abanoubmedhatseif <abanoub.medhat.seif@gmail.com>
2024-04-08 02:20:27 +0200

    removed unnecessary snippits

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -16,9 +16,14 @@ const ProductCard = ({ data }) => {
             key={item.id}
           >
             {/* Favorite icon */}
-            <div className="absolute top-0 right-1 m-2">
-              <FontAwesomeIcon icon={faHeart} style={{ color: "white" }} />
-            </div>
+            {/* i will move it into product details page */}
+            {/* <div className="absolute top-0 right-1 m-2">
+              <FontAwesomeIcon
+                icon={faHeart}
+                style={{ color: "white" }}
+                size="lg"
+              />
+            </div> */}
 
             <div>
               <img
@@ -29,7 +34,7 @@ const ProductCard = ({ data }) => {
               {/* hover button */}
               <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
                 <Button
-                  text={"Add to cart"}
+                  text={"View details"}
                   bgColor={"bg-primary"}
                   textColor={"text-white"}
                 />
@@ -43,40 +48,6 @@ const ProductCard = ({ data }) => {
         ))}
       </div>
     </div>
-
-    // <div className="mb-10">
-    //   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 place-items-center mt-10 px-8 ">
-    //     {/* card section */}
-    //     {data.map((data) => (
-    //       <div
-    //         data-aos="fade-up"
-    //         data-aos-delay={data.aosDelay}
-    //         className="group"
-    //         key={data.id}
-    //       >
-    //         <div className="relative">
-    //           <img
-    //             src={data.img}
-    //             alt=""
-    //             className="h-[180px] w-[260px] object-cover rounded-md"
-    //           />
-    //           {/* hover button */}
-    //           <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
-    //             <Button
-    //               text={"Add to cart"}
-    //               bgColor={"bg-primary"}
-    //               textColor={"text-white"}
-    //             />
-    //           </div>
-    //         </div>
-    //         <div className="leading-7">
-    //           <h2 className="font-semibold">{data.title}</h2>
-    //           <h2 className="font-bold">${data.price}</h2>
-    //         </div>
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
   );
 };
 
