08f9da7487ae2820cf67b2805960561f7ec3129c
Abanoubmedhatseif <abanoub.medhat.seif@gmail.com>
2024-04-09 22:27:57 +0200

    merged cart

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/productSlice";
 import productDetailsReducer from "./slices/productDetailsSlice";
 import wishlistReducer from "./slices/wishlistSlice";
+import cartReducer from "./slices/cartSlice";
 
 export default configureStore({
   reducer: {
     product: productReducer,
     productDetails: productDetailsReducer,
     wishlist: wishlistReducer,
+    cart: cartReducer,
   },
 });
