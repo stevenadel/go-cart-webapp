ced7d6850f5d1b1652d0794ec9b7923f350b46d9
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-09 17:16:06 +0200

    update cart

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import Button from "../reusables/Button";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCartThunk } from "../../store/slices/cartSlice";
 
@@ -11,23 +12,7 @@ import {
   removeFromWishlist,
 } from "../../store/slices/wishlistSlice";
 
-const ProductCard = ({ item }) => {
-  const dispatch = useDispatch();
-
-  const handleAddToWishlist = async (productId) => {
-    await dispatch(addToWishlist(productId));
-  };
-
-  const handleRemoveFromWishlist = async (productId) => {
-    await dispatch(removeFromWishlist(productId));
-  };
-
-  const [isFavourite, setIsFavourite] = useState(false);
-
-  const toggleFavourite = () => {
-    setIsFavourite((prevState) => !prevState);
-  };
-
+const ProductCard = ({ data }) => {
   const dispatch = useDispatch();
 
   const handleAddToCart = (productId) => {
@@ -43,55 +28,36 @@ const ProductCard = ({ item }) => {
   };
 
   return (
-    <div>
-      <div className="group relative" key={item.id}>
-        <div>
-          <img
-            src={item.img}
-            alt=""
-            className="h-[180px] w-[260px] object-cover rounded-md"
-          />
-          {/* hover button */}
-          <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
-            <Button
-              text={"View details"}
-              bgColor={"bg-primary"}
-              textColor={"text-white"}
-            />
+    <div className="mb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 place-items-center mt-10 px-8 ">
+        {data.map((product) => (
+          <div
+            data-aos="fade-up"
+            data-aos-delay={product.aosDelay}
+            className="group"
+            key={product.id}
+          >
+            <div className="relative">
+              <img
+                src={product.image}
+                alt=""
+                className="h-[180px] w-[260px] object-cover rounded-md"
+              />
+              <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
+                <Button
+                  text={"Add to cart"}
+                  bgColor={"bg-primary"}
+                  textColor={"text-white"}
+                  handler={() => handleAddToCart(product.id)}
+                />
+              </div>
+            </div>
+            <div className="leading-7">
+              <h2 className="font-semibold">{product.name}</h2>
+              <h2 className="font-bold">${product.price}</h2>
+            </div>
           </div>
-        </div>
-        <div className="leading-7">
-          <h2 className="font-semibold">{item.title}</h2>
-          <h2 className="font-bold">${item.price}</h2>
-        </div>
-      </div>
-      <div style={{ marginTop: 5, marginBottom: 20 }}>
-        <button className="bg-primary text-white px-4 py-2 rounded-md">
-          Add to Cart
-        </button>
-        <button
-          style={{
-            display: "inline-block",
-            verticalAlign: "middle",
-            marginLeft: "15px",
-          }}
-          onClick={() => {
-            toggleFavourite();
-            if (isFavourite) {
-              handleRemoveFromWishlist(item.id);
-            } else {
-              handleAddToWishlist(item.id);
-            }
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faHeart}
-            size="lg"
-            style={{
-              color: isFavourite ? "red" : "grey",
-            }}
-          />
-        </button>
+        ))}
       </div>
     </div>
   );
